4d88415b18d0e1bd0acd8669571f49f5b178abb1
octavian-bejan <bejan.octavian@yahoo.ro>
2017-01-11 18:56:53 +0200

    Afisare
    Afisari unitati de masura , modificari la afisari ale u.m.

--- Source.cpp
+++ Source.cpp
@@ -32,56 +32,81 @@ void afisare_optiuni()
 }
 void afisare_optiuniLungime()
 {
-	cout << "1. Milimetri." << endl;
-	cout << "2. Centimetri." << endl;
-	cout << "3. Metri." << endl;
-	cout << "4. Kilometri." << endl;
-	cout << "5. Inch." << endl;
-	cout << "6. Picioare(Feets)." << endl;
-	cout << "7. Yarzi." << endl;
-	cout << "8. Mila." << endl;
-	cout << "9. Mila maritima." << endl;
+	cout << "1. mm" << endl;
+	cout << "2. cm" << endl;
+	cout << "3. m" << endl;
+	cout << "4. km" << endl;
+	cout << "5. Inchs." << endl;
+	cout << "6. ft" << endl;
+	cout << "7. yz" << endl;
+	cout << "8. mi" << endl;
+	cout << "9. nmi" << endl<<endl;
+	cout<<"mm->Milimetri   cm->Centimetri   m->Metri   km->Kilometri"<<endl;
+	cout<<"ft->Picioare(Feets)   yz->Yarzi    mi->Mila   nmi->Mila nautica"<<endl;
 }
 void afisare_optiuniArie()
 {
-    cout<<"1. Ari."<<endl;
-    cout<<"2. Acri."<<endl;
-    cout<<"3. Hectare."<<endl;
-    cout<<"4. Milimetri"<<square<<"."<<endl;
-    cout<<"5. Centimetri"<<square<<"."<<endl;
-    cout<<"6. Metri"<<square<<"."<<endl;
-    cout<<"7. Kilometri"<<square<<"."<<endl;
-    cout<<"8. Picioare"<<square<<"(Feets"<<square<<")."<<endl;
-    cout<<"9. Inches"<<square<<"."<<endl;
-    cout<<"10. Yarzi"<<square<<"."<<endl;
+    cout<<"1. a"<<endl;
+    cout<<"2. ac"<<endl;
+    cout<<"3. ha"<<endl;
+    cout<<"4. mm"<<square<<endl;
+    cout<<"5. cm"<<square<<endl;
+    cout<<"6. m"<<square<<endl;
+    cout<<"7. km"<<square<<endl;
+    cout<<"8. ft"<<square<<endl;
+    cout<<"9. Inches"<<square<<endl;
+    cout<<"10. yz"<<square<<endl<<endl;
+    cout<<"a->Ari   ac->Acri   ha->Hectari   mm->Milimetri   cm->Centimetri"<<endl;
+    cout<<"m->Metri  km->Kilometri   ft->Picioare(Feets)   yz->Yarzi"<<endl;
 }
 void afisare_optiuniVolum()
 {
-    cout<<"1. Mililitri."<<endl;
-    cout<<"2. Centilitri."<<endl;
-    cout<<"3. Decilitri."<<endl;
-    cout<<"4. Litri."<<endl;
+    cout<<"1. ml"<<endl;
+    cout<<"2. cl"<<endl;
+    cout<<"3. dl"<<endl;
+    cout<<"4. l"<<endl;
     cout<<"5. Gal(UK)."<<endl;
     cout<<"6. Gal(US)."<<endl;
-    cout<<"7. Centimetri"<<cube<<"."<<endl;
-    cout<<"8. Decimetri"<<cube<<"."<<endl;
-    cout<<"9. Metri"<<cube<<"."<<endl;
-    cout<<"10. Picioare"<<cube<<"(Feets"<<cube<<")."<<endl;
-    cout<<"11. Inches"<<cube<<"."<<endl;
-    cout<<"12. Yarzi"<<cube<<"."<<endl;
+    cout<<"7. cm"<<cubeendl;
+    cout<<"8. dm"<<cube<<endl;
+    cout<<"9. m"<<cube<<endl;
+    cout<<"10. ft"<<cube<<endl;
+    cout<<"11. Inches"<<cube<<endl;
+    cout<<"12. yz"<<cube<<endl<<endl;
+    cout<<"ml->Mililitri    cl->Centilitri   dl->decilitri   l->Litri"<<endl;
+    cout<<"cm->Centimetri   dm->Decimetri    m->Metri    ft->Picioare(Feets)    yz->Yarzi"<<endl;
 }
 void afisare_optiuniTimp()
 {
-    cout<<"1. Saptamana."<<endl;
-    cout<<"2. Zi."<<endl;
-    cout<<"3. Ora."<<endl;
-    cout<<"4. Minut."<<endl;
-    cout<<"5. Secunda."<<endl;
-    cout<<"6. Milisecunda."<<endl;
+    cout<<"1. w"<<endl;
+    cout<<"2. d"<<endl;
+    cout<<"3. h"<<endl;
+    cout<<"4. min"<<endl;
+    cout<<"5. s"<<endl;
+    cout<<"6. ms"<<endl<<endl;
+    cout<<"w->Saptamana   d->Zi   h->Ora    min->Minut"<<endl;
+    cout<<"s->Secunda    ms->Milisecunda"<<endl;
 }
 void afisare_optiuniViteza()
 {
-
+    cout<<"1. cm/min"<<endl;
+    cout<<"2. cm/s"<<endl;
+    cout<<"3. fph"<<endl;
+    cout<<"4. fps"<<endl;
+    cout<<"5. ips"<<endl;
+    cout<<"6. km/h"<<endl;
+    cout<<"7. km/min"<<endl;
+    cout<<"8. km/s"<<endl;
+    cout<<"9. m/h"<<endl;
+    cout<<"10. m/min"<<endl;
+    cout<<"11. mps"<<endl;
+    cout<<"12. mph"<<endl;
+    cout<<"13. mi/min"<<endl;
+    cout<<"14. mi/s"<<endl;
+    cout<<"15. nmi/h"<<endl<<endl;
+    cout<<"cm->Centimetru    fph->Picioare/Ora   fps->Picioare/Secunda    ips->Inch/Secunda"<<endl;
+    cout<<"km->Kilometru    m->Metru    mps->Metru/Secunda   mph->Mila/Ora     mi->Mila"<<endl;
+    cout<<"nmi->Mila nautica    min->Minut   s->Secunda     h->Ora   "<<endl;
 }
 void afisare_optiuniTemperatura()
 {
@@ -93,29 +118,72 @@ void afisare_optiuniTemperatura()
 }
 void afisare_optiuniMasa()
 {
-    cout<<"1. Miligrame."<<endl;
-    cout<<"2. Grame."<<endl;
-    cout<<"3. Kilograme."<<endl;
+    cout<<"1. mg"<<endl;
+    cout<<"2. g"<<endl;
+    cout<<"3. kg"<<endl;
     cout<<"4. Tone."<<endl;
-    cout<<"5. Funte(pount)"<<endl;
-    cout<<"6. Oz(AV),Uncii(Avoirdupois). "<<endl;
-    cout<<"7. Oz(Troy),Uncii(Troy)."<<endl;
+    cout<<"5. lb"<<endl;
+    cout<<"6. oz(AV)"<<endl;
+    cout<<"7. oz(Troy)"<<endl<<endl;
+    cout<<"mg->Miligrame    g->Grame    kg->kilograme    t->Tone "<<endl;
+    cout<<"lb->Funte(pound)     oz(AV)->Uncii(Avoirdupois)     oz(Troy)->Uncii(Troy)"<<endl;
 }
 void afisare_optiuniEnergie()
 {
-
+    cout<<"1. cal"<<endl;
+    cout<<"2. kcal"<<endl;
+    cout<<"3. J"<<endl;
+    cout<<"4. KJ"<<endl;
+    cout<<"5. W"<<endl;
+    cout<<"6. W.s"<<endl;
+    cout<<"7. W.h"<<endl;
+    cout<<"8. Kw.h"<<endl;
+    cout<<endl;
+    cout<<"cal->Calorie    kcal->Kilocalorie    J->Jouli     KJ->KiloJouli"<<endl;
+    cout<<"W->Watt   W.s->Watt secunda   W.h->Watt ora    Kw.h->Kilowatt ora"<<endl;
 }
 void afisare_optiuniPresiune()
 {
-
+    cout<<"1. at"<<endl;
+    cout<<"2. atm"<<endl;
+    cout<<"3. mbar"<<endl;
+    cout<<"4. bar"<<endl;
+    cout<<"5. Pa"<<endl;
+    cout<<"6. KPa"<<endl;
+    cout<<"7. MPa"<<endl;
+    cout<<"8. N"<<endl;
+    cout<<"9. Torr"<<endl;
+    cout<<"10. Psi"<<endl;
+    cout<<endl;
+    cout<<"at->Atmosfera tehnica    atm->Atmosfera standard     bar->Bar    mbar->Milibar"<<endl;
+    cout<<"KPa->Kilopascal   Pa->Pascal   MPa->Megapascal     N->Newton    Torr->Torr"<<endl;
+    cout<<"Psi->Pounds/Inch"<<square<<endl;
 }
 void afisare_optiuniDensitate()
 {
-
+    cout<<"1. g/cm"<<cube<<endl;
+    cout<<"2. g/l"<<endl;
+    cout<<"3. g/m"<<cube<<endl;
+    cout<<"4. Kg/cm"<<cube<<endl;
+    cout<<"5. Kg/l"<<endl;
+    cout<<"6. Kg/m"<<cube<<endl;
+    cout<<"7. t/m"<<cube<<endl;
+    cout<<"8. lb/ft"<<cube<<endl;
+    cout<<"9. lb/in"<<cube<<endl;
+    cout<<"10. lb/gal(US)"<<endl;
+    cout<<"11. lb/gal(UK)"<<endl<<endl;
+    cout<<"g->gram   Kg->Kilogram   t->tona   lb->Fonta(pound)"<<endl;
+    cout<<"cm->Centimetru   l->Litru   m->Metru   ft->Picioare(Feets)  "<<endl;
+    cout<<" in->Inches   gal->Galon"<<endl;
 }
 void afisare_optiuniConsum()
 {
-
+    cout<<"1. l/100km"<<endl;
+    cout<<"2. mi/gal(US)"<<endl;
+    cout<<"3. mi/gal(UK)"<<endl;
+    cout<<"4. km/l"<<endl;
+    cout<<endl;
+    cout<<"l->Litru     km->Kilometru     mi->Mila      gal->Galon"<<endl;
 }
 
 /*
